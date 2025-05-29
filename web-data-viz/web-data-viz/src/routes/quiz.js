a65099f6d4bf11ee976cf38151a879d29ec1b87f
rafael <rafael.vieira@sptech.school>
2025-05-29 09:40:20 -0300

    atualiza configurações de ambiente, altera estilos para cores mais claras e adiciona rota para obter resultados do quiz

--- web-data-viz/web-data-viz/src/routes/quiz.js
+++ web-data-viz/web-data-viz/src/routes/quiz.js
@@ -4,6 +4,8 @@ var quizController = require('../controllers/quizController');
 
 // Rota POST para finalizar o quiz
 router.post('/finalizar', quizController.finalizarQuiz);
+
+// Rota get para colotar os dados do quiz
 router.get('/resultados', quizController.obterResultados);
 router.get('/resultado-usuario/:idUsuario', quizController.obterResultadoUsuario);
 
